5867ac76143fd3d1abe10bbb585d61bba5db7ef1
rafaellepalmos <rafaelle.palmos@students.stamford.edu>
2015-11-25 09:52:23 +0700

    week2 wed hw1

--- Week2/JS/add-content.js
+++ Week2/JS/add-content.js
@@ -1,11 +1,37 @@
-var price;
-var quantity;
-var total;
+var products = ["Stamford T-Shirt", "Stamford Notebook", "Stamford Wristband"];
+var price = [10, 5, 1];
+var total = 0;
 
-price = 5;
-quantity = 14;
-total = price * quantity;
+hour = new Date().getHours();
 
-//document.write("Total is " + total);
+if (hour < 12) {
+    greeting = "Good Morning";
+} else if (hour > 17) {
+	greeting = "Good Evening";
+} else {
+	greeting = "Good Afternoon";
+}
+
+var greetingEle = document.getElementById("greeting");
+greetingEle.textContent = greeting;
+
+name = "Rafaelle";
+
+var nameEle = document.getElementById("name");
+nameEle.textContent = name;
+
+//Calculate the total price below
+
+for (i=0; i < 3; i++) {
+	var productList = document.getElementById("productList");
+	productList.innerHTML += "<li>" +products[i] + ": $" + price[i] +"</li>";
+
+	//Display purchased product to the screen
+	total += price[i];
+}
+
+//20% discount
+total = total*0.8;
+
 var totalPriceEle = document.getElementById("totalPrice");
 totalPriceEle.textContent = "$" + total;
