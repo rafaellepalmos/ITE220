44a533d2bed758ca9f6f44e8c92df13accd8f755
rafaellepalmos <rafaelle.palmos@students.stamford.edu>
2015-11-26 09:37:01 +0700

    week2 wed hw2 for loop

--- Week2-2/js/calendar.js
+++ Week2-2/js/calendar.js
@@ -1,6 +1,14 @@
 // display the calendar here
 var hotelTable = document.getElementById("hotelCalendarTable");
 var defaultData = hotelTable.innerHTML;
-var stringHTML = "";
-stringHTML = "<tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td></tr><tr><td>9</td><td>10</td><td>11</td><td>12</td><td>13</td><td>14</td><td>15</td></tr><tr><td>17</td><td>18</td><td>19</td><td>20</td><td>21</td><td>22</td><td>23</td></tr><tr><td>25</td><td>26</td><td>27</td><td>28</td><td>29</td><td>30</td><td>31</td></tr>";
+var stringHTML = "<tr>";
+
+for (var i=1; i < 32; i++) {
+	if (i%7 == 0){
+		stringHTML += "<td>"+i+"</td></tr><tr>";
+	} else {
+		stringHTML += "<td>"+i+"</td>";
+	}
+}
+//stringHTML += "</tr>"
 hotelTable.innerHTML += stringHTML;
