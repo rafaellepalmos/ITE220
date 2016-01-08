cee78f529b9b6aa5fce2742e8bbe68125e060565
rafaellepalmos <rafaelle.palmos@students.stamford.edu>
2016-01-08 22:06:30 +0700

    week7 hw

---
+++ Week7/js/app.js
@@ -0,0 +1,48 @@
+//Rafaelle Palmos 214210055
+$(function(){
+	var changeUnit = "";
+	var unit = " F";
+	var city = "";
+	function ajaxcall(){
+		$.ajax({
+			type: 'GET',
+			url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + changeUnit + '&appid=2de143494c0b295cca9337e1e96b00e0',
+			success: function(data){
+				console.log(data.name);
+				$("#container").html("Temperature: " + data.main.temp + unit + "<br>Pressure: " + data.main.pressure + " hPa<br>Humidity: " + data.main.humidity + "%");	
+			}
+		});
+	}
+	$("#cities>a").on("click", function(e){
+		e.preventDefault();
+		//alert(this.text);
+		city = this.text.toLowerCase();
+		/*$.ajax({
+			type: 'GET',
+			url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + changeUnit + '&appid=2de143494c0b295cca9337e1e96b00e0',
+			success: function(data){
+				console.log(data.name);
+				$("#container").html(data.name + ": " + data.main.temp + unit);	
+			}
+		});*/
+		ajaxcall();
+	});
+	$("#fahrenheit").on("click", function(){
+		changeUnit = "";
+		unit = " F";
+		if(city != ""){
+			ajaxcall();
+		} else{
+			sweetAlert("Oops", "please choose a city", "error");
+		}
+	});
+	$("#celsius").on("click", function(){
+		changeUnit = "&units=metric";
+		unit = " C";
+		if(city != ""){
+			ajaxcall();
+		} else{
+			sweetAlert("Oops", "please choose a city", "error");
+		}
+	});
+});
